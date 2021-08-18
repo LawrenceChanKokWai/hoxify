4bbf8d95bed0aa0d374234596e7f979337583c91
LawrenceChanKokWai <68629118+LawrenceChanKokWai@users.noreply.github.com>
2021-08-18 23:10:32 +0800

    Add files via upload

---
+++ blog-backend/src/main/java/com/revision/blogpractice/shared/GenericResponse.java
@@ -0,0 +1,21 @@
+package com.revision.blogpractice.shared;
+
+public class GenericResponse {
+
+    private String message;
+
+    public GenericResponse() {
+    }
+
+    public GenericResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
